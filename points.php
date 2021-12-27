7ac94d05621fbcd346f13e1364b7b3fc6f84be0a
Shiv Yadav <shiv@cibirix.com>
2021-12-27 10:40:29 +0530

    points

---
+++ points.php
@@ -0,0 +1,9 @@
+Fragments
+Portals
+Context
+Suspense
+Error Boundaries
+Lazy Components
+Async and Concurrent Rendering
+Implicit Event Delegation
+SSR and Hydration(although there is no Next.js equivalent yet).
